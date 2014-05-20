321aa918370de9c87d73ef58fa3b7d9594eea18d
Lokidottir <mari.harald@gmail.com>
2014-05-20 22:48:43 +0100

    Expanded maths::Vector header
    Moved the majority of the already written Vector members over to the
    header file, as templates cause some issues & forward declaration is
    currently not necessary.

--- bop-maths/Vector.hpp
+++ bop-maths/Vector.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 /*
-	bop::maths::Vector forward declaration file
+	bop::maths::Vector Class file
 */
 
 namespace bop::maths {
@@ -18,17 +18,126 @@ namespace bop::maths {
 		public:
 			T* data;
 			//Constructors
-			Vector(int, T);
-			Vector(int, int, T);
-			Vector(std::initializer_list<T>);
-			Vector();
+			Vector(int width, T fill = 0) {
+				/*
+					Creates a new vector of a given width, setting each element as a given default.
+					----pivot index is set as -1.
+				*/
+				this->pivot_index = -1;
+				this->data = new T[width];
+				this->width = width;
+				for (int i = 0; i < this->width; i++) {
+					this->data[i] = fill;
+				}
+			}
+			
+			Vector(std::initializer_list<T> list) {
+				/*
+					Creates a new Vector from an initializer list.
+					----pivot_index is determined during construction.
+					----width is determined from size of list.
+				*/
+				this->width = list.size();
+				this->pivot_index = -1;
+				this->data = new T[this->width];
+				int i = 0;
+				for (T elem : list) {
+					this->data[i] = elem;
+					if (this->pivot_index < 0 && elem != 0) {
+						this->pivot_index = i;
+					}
+					i++;
+				}
+			}
+			
+			Vector(const Vector<T>& vec) {
+				/*
+					Creates a new Vector from a Vector, a copy constructor.
+					----copies all data from the Vector, incl. deep copy of data array pointer.
+				*/
+				this->width = vec.width;
+				this->pivot_index = vec.pivot_index;
+				this->data = new T[this->width];
+				for (int i = 0; i < this->width; i++) {
+					this->data[i] = vec.data[i];
+				}
+			}
+			
+			Vector() {
+				/*
+					Empty constructor, does nothing.
+				*/
+			}
+			
+			/*
+				Todo: Vector(Vector&& vec) move constructor
+			*/
+			
 			//Operator Overloads
-			T& operator[] (const unsigned int);
-			Vector<T>& operator= (const Vector<T>&);
-			Vector<T>& operator= (std::initializer_list<T>);
+			
+			T& operator[] (const unsigned int index) {
+				/*
+					Index operator, returns the data at the given index.
+					----synonymous to Vector::data[const unsigned int]
+				*/
+				return this->data[index];
+			}
+			
+			Vector<T>& operator= (const Vector<T>& vec) : Vector(vec) {
+				/*
+					Copy assignment, calls copy constructor.
+					----synonymous to the copy constructor
+				*/
+				return *this;
+			}
+			
+			Vector<T>& operator= (std::initializer_list<T> list) {
+				/*
+					Initializer list assignment operator, synonymous
+					with the constructor that takes the same argument.
+					----copied here so the list is not passed by value
+				*/
+				this->width = list.size();
+				this->pivot_index = -1;
+				this->data = new T[this->width];
+				int i = 0;
+				for (T elem : list) {
+					this->data[i] = elem;
+					if (this->pivot_index < 0 && elem != 0) {
+						this->pivot_index = i;
+					}
+					i++;
+				}
+				return *this;
+			}
+			
 			//Arithmetic Operator Overloads
-			Vector<T>& operator*= (const T);
-			Vector<T>& operator/= (const T);
+			
+			Vector<T>& operator*= (const T scalar) {
+				/*
+					Member multiplication operator, multiplies all
+					elements in a vector by the given scalar.
+				*/
+				if (scalar == 0) return *this;
+				for (int i = 0; i < this->width; i++) {
+					this->data[i] *= scalar;
+				}
+				return *this;
+			}
+			
+			Vector<T>& operator/= (const T scalar) {
+				/*
+					Member division operator, divides all 
+					elements in a vector by the given scalar.
+					----synonymous with "vec *= 1/scalar"
+				*/
+				if (scalar == 0) return *this;
+				for (int i = 0; i < this->width; i++) {
+					this->data[i] /= scalar;
+				}
+				return *this;
+			}
+			
 			Vector<T>& operator+= (Vector<T>&);
 			Vector<T>& operator-= (Vector<T>&);
 			//Vector information functions
