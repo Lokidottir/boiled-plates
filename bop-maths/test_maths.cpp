dcc113c1557a5532cace81ddf42122ff2f80775a
Lokidottir <mari.harald.uk@gmail.com>
2014-05-26 22:04:59 +0100

    Finished maths::Vector portion of test env

--- bop-maths/test_maths.cpp
+++ bop-maths/test_maths.cpp
@@ -7,6 +7,7 @@ int testVectors() {
 	/*
 		Testing Operators
 	*/
+	std::cout << "\n####\nOperator Tests\n####" << std::endl;
 	Vector<double> vec3 = {4,7,2};
 	std::cout << "vector " << vec3.string() << " multiplied by 4: " << (vec3 * 4.0).string() << std::endl;
 	std::cout << "vector " << vec3.string() << " divided by 4: " << (vec3 / 4.0).string() << std::endl;
@@ -16,6 +17,7 @@ int testVectors() {
 	/*
 		Testing Swap
 	*/
+	std::cout << "\n####\nSwap Tests\n####" << std::endl;
 	std::cout << "I shall perform a magic trick! Watch as I change the positions of these vectors!" << std::endl;
 	Vector<double> vec8 = {1,2,3,4,5,6,7,8};
 	Vector<double> vec4 = {9,10,11,12};
@@ -26,10 +28,32 @@ int testVectors() {
 	/*
 		Testing coords
 	*/
-	std::cout << "coordinate vals of " << vec3.string() << " are x: " << vec3.x() << " y: " << vec3.y() << " z: " << vec3.z() << " with a w of: " << vec3.w() << std::endl;
+	std::cout << "\n####\nCoordinate Tests\n####" << std::endl;
+	std::cout << "Coordinate vals of " << vec3.string() << " are x: " << vec3.x() << " y: " << vec3.y() << " z: " << vec3.z() << " with a w of: " << vec3.w() << std::endl;
 	/*
-		
+		Pivot test
 	*/
+	std::cout << "\n####\nPivot Tests\n####" << std::endl;
+	Vector<double> vec_pivot = {0,0,5,3};
+	std::cout << "The pivot col of vector " << vec_pivot.string() << " is: " << vec_pivot.pivot() << std::endl;
+	/*
+		Bool test
+	*/
+	std::cout << "\n####\nBoolean Tests\n####" << std::endl;
+	Vector<double> vec_bool_1 = {4,2,1};
+	Vector<double> vec_bool_2 = {4,2,1};
+	Vector<double> vec_bool_3 = {9,3,1};
+	std::cout << "For the following vectors: " << vec_bool_1.string() << ", " << vec_bool_2.string() << ", " << vec_bool_3.string() <<std::endl;
+	std::cout << "(vec_bool_1 == vec_bool_2) evaluates to " << (vec_bool_1 == vec_bool_2) << std::endl;
+	std::cout << "(vec_bool_1 != vec_bool_2) evaluates to " << (vec_bool_1 != vec_bool_2) << std::endl;
+	std::cout << "(vec_bool_2 == vec_bool_3) evaluates to " << (vec_bool_2 == vec_bool_3) << std::endl;
+	std::cout << "(vec_bool_2 != vec_bool_2) evaluates to " << (vec_bool_2 != vec_bool_3) << std::endl;
+	/*
+		Size test
+	*/
+	std::cout << "\n####\nSize Tests\n####" << std::endl;
+	Vector<double> vec_size = {5,6,9,3,8,5,7,11,993,1232,-6,2,1.000005,2};
+	std::cout << "Size of vector " << vec_size.string() << " is " << vec_size.size() << std::endl;
 	return 0;
 }
 
