7abb497e9cf0b6f6a2336324f6af8be752aa73ff
Lokidottir <mari.harald.uk@gmail.com>
2014-05-28 13:13:24 +0100

    Started maths::Matrix testing

--- bop-maths/test_maths.cpp
+++ bop-maths/test_maths.cpp
@@ -58,7 +58,51 @@ int testVectors() {
 	return 0;
 }
 
+int testMatrices() {
+	std::cout << "####\n####\nmaths::bop::Matrix testing\n####\n####" << std::endl;
+	/*
+		Operator test
+	*/
+	std::cout << "\n####\nOperator Tests\n####" << std::endl;
+	Matrix<double> mat1 = {{2,6,-1},{-1,9,6},{4,2,9}};
+	Matrix<double> mat2 = {{3,7,2},{-1,-1,-1},{5,2,-6}};
+	std::cout << "Matrix 1:" << std::endl << mat1.string() << std::endl;
+	std::cout << "Matrix 1 multiplied by 4:" << std::endl << (mat1 * 4.0).string() << std::endl;
+	std::cout << "Matrix 1 divided by 4:" << std::endl << (mat1 / 4.0).string() << std::endl;
+	std::cout << "Matrix 2:" << std::endl << mat2.string() << std::endl;
+	std::cout << "Matrix 1 multiplied by Matrix 2:" << std::endl << (mat1 * mat2).string() << std::endl;
+	std::cout << "Matrix 1 add Matrix 2:" << std::endl << (mat1 + mat2).string() << std::endl;
+	std::cout << "Matrix 1 subtract Matrix 2:" << std::endl << (mat1 - mat2).string() << std::endl;
+	/*
+		Swap tests
+	*/
+	std::cout << "\n####\nSwap Tests\n####" << std::endl;
+	std::cout << "Matrix 1:" << std::endl << mat1.string() << std::endl << "to be swapped with Matrix 2:" << std::endl << mat2.string() << std::endl;
+	mat1.swap(mat2);
+	std::cout << "Matrix 1:" << std::endl << mat1.string() << std::endl << "Matrix 2:" << std::endl << mat2.string() << std::endl;
+	std::cout << "swapping rows 1 & 3 (non-zero index) of matrix 1:" << std::endl << mat1.string() <<std::endl;
+	mat1.swapRows(0,2);
+	std::cout << "Matrix 1 is now:" << std::endl << mat1.string();
+	/*
+		Bool test
+	*/
+	std::cout << "\n####\nBoolean Tests\n####" << std::endl;
+	Matrix<double> bool_mat1 = {{3,7},{-1,2}};
+	Matrix<double> bool_mat2 = {{3,7},{-1,2}};
+	Matrix<double> bool_mat3 = {{10,3},{5,-7}};
+	std::cout << "Bool matrix 1:" << std::endl << bool_mat1.string() << std::endl;
+	std::cout << "Bool matrix 2:" << std::endl << bool_mat2.string() << std::endl;
+	std::cout << "Bool matrix 3:" << std::endl << bool_mat3.string() << std::endl;
+	std::cout << "(mat1 == mat2) evaluates to: " << (bool_mat1 == bool_mat2) << std::endl;
+	std::cout << "(mat1 != mat2) evaluates to: " << (bool_mat1 != bool_mat2) << std::endl;
+	std::cout << "(mat2 == mat3) evaluates to: " << (bool_mat2 == bool_mat3) << std::endl;
+	std::cout << "(mat2 != mat3) evaluates to: " << (bool_mat2 != bool_mat3) << std::endl;
+	
+	return 0;
+}
+
 int main() {
 	std::cout << "Vector test returned " << testVectors() << std::endl;
+	std::cout << "Matrix test returned " << testMatrices() << std::endl;
 	return 0;
 }
